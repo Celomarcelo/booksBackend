55108fbc2d9f529bb0c9fef93d6fd197f1e02c91
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-11-09 12:09:18 +0000

    updated cors allowed origins

--- review_platform/settings.py
+++ review_platform/settings.py
@@ -68,11 +68,12 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000", 
-    "new-books-frontend-fea3df7794e6.herokuapp.com", 
+    "http://new-books-frontend-fea3df7794e6.herokuapp.com/", 
 ]
 
 CORS_ALLOW_HEADERS = ['*']
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+CORS_ALLOW_CREDENTIALS = True
 
 
 MIDDLEWARE = [
